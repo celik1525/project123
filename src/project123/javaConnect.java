c968cf635fdfc231654d03e7ced808e45136187a
Ruhi ÇELİK <Ruhi ELK@DESKTOP-F6HDPRQ>
2017-08-16 20:30:14 +0300

    ilk git çalışması

---
+++ src/project123/javaConnect.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project123;
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author Ruhi ÇELİK
+ */
+
+public class javaConnect {
+    Connection conn=null;
+    
+    public static Connection ConnectDb() {
+        try{
+        Class.forName("org.sqlite.JDBC");
+        Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Ruhi ÇELİK\\Documents\\NetBeansProjects\\Projec123JPA\\Project123.sqlite");
+          
+        return conn;
+        }catch(Exception e){
+         JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+        
+        
+    }
+    public static String normTablo;
+    public static int sicil;
+
+}
